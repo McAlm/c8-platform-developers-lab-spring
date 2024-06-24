6e1fb6698a7a4c64ccab7ed54a3ede1f23469df4
McAlm <stefan.wiese@camunda.com>
2024-06-24 15:09:37 +0200

    exercise-6

--- src/main/java/com/camunda/academy/worker/ChargeCreditCardWorker.java
+++ src/main/java/com/camunda/academy/worker/ChargeCreditCardWorker.java
@@ -1,16 +1,33 @@
 package com.camunda.academy.worker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.camunda.academy.services.CreditCardService;
+
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import io.camunda.zeebe.spring.client.annotation.Variable;
 import lombok.extern.java.Log;
 
 @Component
 @Log
 public class ChargeCreditCardWorker {
 
-    @JobWorker(type="credit-card-charging")
-    public void chargeCreditCard(){
+    @Autowired
+    private CreditCardService ccs;
+
+    @JobWorker(type = "credit-card-charging", //
+            fetchVariables = { //
+                    "cardNumber", //
+                    "cvc", //
+                    "expiryDate", //
+                    "openAmount" })
+    public void chargeAmount(//
+            @Variable String cardNumber, //
+            @Variable String cvc, //
+            @Variable String expiryDate, //
+            @Variable Double openAmount) {
         log.info("Charging credit card");
+        ccs.chargeAmount(cardNumber, cvc, expiryDate, openAmount);
     }
 }
--- src/main/java/com/camunda/academy/worker/DeductCreditWorker.java
+++ src/main/java/com/camunda/academy/worker/DeductCreditWorker.java
@@ -1,16 +1,27 @@
 package com.camunda.academy.worker;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.camunda.academy.services.CustomerService;
+
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import io.camunda.zeebe.spring.client.annotation.Variable;
 import lombok.extern.java.Log;
 
 @Component
 @Log
 public class DeductCreditWorker {
 
-    @JobWorker(type="credit-deduction")
-    public void creditDeduction(){
-        log.info("Deducting Customer Credit");
+    @Autowired
+    private CustomerService cs;
+
+    @JobWorker(type="credit-deduction", fetchAllVariables = false)
+    public Map<String, Object> creditDeduction(@Variable String customerId, @Variable Double orderTotal){
+        log.info("Deducting Customer Credit for Customer " + customerId + " and OrderTotal " + orderTotal) ;
+        Double openAmount = cs.deductCredit(customerId, orderTotal);
+        return Map.of("openAmount", openAmount);
     }
 }
